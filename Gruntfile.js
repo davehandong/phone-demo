ade80a8d2bb815d9f1315fda3d2895cb0cdd56cb
davehandong <davehandong@163.com>
2016-03-21 17:37:49 +0800

    Gruntfile add uglify.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,34 @@
 module.exports = function(grunt){
   grunt.initConfig({
     sass: {
-      dist: {
+      dist_expanded: {
         options:{
           style: 'expanded'
         },
         files: {
           'assets/bootstrap/css/bootstrap.css': 'assets/bootstrap/bootstrap.scss'
         }
+      },
+      dist_compressed: {
+        options:{
+          style: 'compressed'
+        },
+        files: {
+          'assets/bootstrap/css/bootstrap.min.css': 'assets/bootstrap/bootstrap.scss'
+        }
       }
-    }
+    },
+    uglify: {
+	  dist_dialog: {
+	    files: {
+	      'assets/js/dialog.min.js': ['assets/js/dialog.js'],
+  	    },
+	  }
+	}
   });
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default',['sass']);
+  grunt.registerTask('default',['sass','uglify']);
 }
