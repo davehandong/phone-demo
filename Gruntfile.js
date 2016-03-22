f3b8bf09c4bb46bdf5fd5576b071a2986a9c5ac5
davehandong <davehandong@163.com>
2016-03-22 11:06:42 +0800

    change direcotry structure, and modify grunt file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,14 @@
 module.exports = function(grunt){
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      map: ['assets/bootstrap/**/*.map','assets/js/**/*.map'],
+      css: ['assets/bootstrap/css/*.css'],
+      js: ['assets/js/dest/*.js']
+    },
+    jshint: {
+      all: ['assets/js/class/*.js']
+    },
     sass: {
       dist_expanded: {
         options:{
@@ -18,17 +27,34 @@ module.exports = function(grunt){
         }
       }
     },
+    concat: {
+      options: {
+        //separator: ';',
+        banner: '/* <%=pkg.name%> - v<%=pkg.version%> - <%=grunt.template.today("yyyy-mm-dd")%> */\n'
+      },
+      dist: {
+        src: ['assets/js/class/alert.js','assets/js/class/creater.js'],
+        dest: 'assets/js/dest/<%=pkg.name%>.js',
+      },
+    },
     uglify: {
-	  dist_dialog: {
-	    files: {
-	      'assets/js/dialog.min.js': ['assets/js/dialog.js'],
-  	    },
-	  }
-	}
+      dist: {
+        options: {
+          mangle: true,
+          preserveComments: 'some',
+          banner: '/* <%=pkg.name%> - v<%=pkg.version%> - <%=grunt.template.today("yyyy-mm-dd")%> */\n'
+        },
+        files: {
+          'assets/js/dest/<%=pkg.name%>.min.js': ['assets/js/dest/<%=pkg.name%>.js'],
+        },
+      }
+    }
   });
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default',['sass','uglify']);
+  grunt.registerTask('default',['clean','jshint','sass','concat','uglify']);
 }
